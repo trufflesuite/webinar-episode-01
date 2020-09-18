92dd79af440b61d00878059c6d3ef9717391b687
Amal Sudama <cds.sudama@gmail.com>
2020-09-17 22:01:39 -0400

    implement initial value

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,4 +2,9 @@
 pragma solidity >=0.5.0 <0.8.0;
 
 contract SimpleStorage {
+    uint256 public storedData;
+
+    function getStoredData() public view returns (uint256) {
+        return storedData;
+    }
 }
