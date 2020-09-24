bae43fbaca44f961939c32a68ddc0bbc55cc1f62
Amal Sudama <cds.sudama@gmail.com>
2020-09-24 18:38:31 -0400

    Implement counter 1

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -4,9 +4,14 @@ pragma solidity >=0.5.0 <0.8.0;
 contract SimpleStorage {
     uint256 public storedData;
     address public owner = msg.sender;
+    mapping(address => uint) public counter;
 
     function getStoredData() public view returns (uint256) {
         return storedData;
+    }
+
+    function getCount(address _address) public view returns (uint) {
+        return counter[_address];
     }
 
     function setStoredData(uint256 x) public {
