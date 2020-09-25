7e2c1aedfa94673719c36078dc4af103baf71f68
Amal Sudama <cds.sudama@gmail.com>
2020-09-24 20:05:57 -0400

    update tests for truffleAssert

--- test/simple_storage.js
+++ test/simple_storage.js
@@ -1,11 +1,13 @@
 const SimpleStorage = artifacts.require("SimpleStorage");
+const {reverts} = require('truffle-assertions');
 
 contract("SimpleStorage", function (accounts) {
 
-  // SimpleStorage is deployed once per contract block
   let ssInstance;
+  const [owner, user] = accounts;
+
   beforeEach( async () => {
-    ssInstance = await SimpleStorage.deployed();
+    ssInstance = await SimpleStorage.new({from: owner});
   });
 
   describe("Initial deployment", async () => {
@@ -24,7 +26,7 @@ contract("SimpleStorage", function (accounts) {
   describe("Functionality", () => {
     it("should store the value 42", async () => {
       // change the subject
-      await ssInstance.setStoredData(42, { from: accounts[0] });
+      await ssInstance.setStoredData(42, { from: user});
 
       // verify we changed the subject
       const storedData = await ssInstance.getStoredData.call();
@@ -34,28 +36,35 @@ contract("SimpleStorage", function (accounts) {
 
   describe("Exercises", () => {
     it("should have an owner", async () => {
-      const owner = await ssInstance.owner.call();
-      assert.equal(owner, accounts[1], "owner should be the deploying address");
+      const contractOwner = await ssInstance.owner.call();
+      assert.equal(contractOwner, owner, "owner should be the deploying address");
     });
 
     describe("Counter", () => {
 
       it("user should default to 0", async () => {
-        const count = await ssInstance.getCount(accounts[9]);
+        const count = await ssInstance.getCount(user);
         assert.equal(count, 0);
       });
 
+      it("non owners not allowed", async() => {
+        await reverts(
+          ssInstance.getCount(user, {from: user}),
+          "restricted to owner"
+        );
+      });
+
       it("tracks user correctly", async () => {
-        await ssInstance.setStoredData(123, { from: accounts[3]});
-        let count = await ssInstance.getCount(accounts[3]);
+        await ssInstance.setStoredData(123, {from: user});
+        let count = await ssInstance.getCount(user);
         assert.equal(count, 1);
 
-        await ssInstance.setStoredData(122, { from: accounts[3]});
-        count = await ssInstance.getCount(accounts[3]);
+        await ssInstance.setStoredData(122, {from: user});
+        count = await ssInstance.getCount(user);
         assert.equal(count, 2);
 
-        await ssInstance.setStoredData(112, { from: accounts[3]});
-        count = await ssInstance.getCount(accounts[3]);
+        await ssInstance.setStoredData(112, {from: user});
+        count = await ssInstance.getCount(user);
         assert.equal(count, 3);
       });
 
